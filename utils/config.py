c67bf581f297b92a1309f2fb58282bf60a03722a
Ashish <ashishworkx@gmail.com>
2026-04-29 19:21:49 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,18 @@
 import os
 
 # Fetching variables from Render Environment
-WP_URL = os.getenv("WP_URL", "https://earnguide.in")
+# Nexovent ki nayi site ka URL yahan default mein rakhein
+WP_URL = os.getenv("WP_URL", "https://dev-nexovent.pantheonsite.io")
+
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-AI_API_KEY = os.getenv("GROQ_API_KEY")
+
+# IS LINE KO DHAYAN SE DEKHO: 
+# Humne variable ka naam wahi rakha hai jo SmartWriter dhoond raha hai
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# Extra Safety: Agar koi AI_API_KEY naam se bhi bulaye toh bhi mil jaye
+AI_API_KEY = GROQ_API_KEY 
 
 # Public settings
 BRAND_NAME = "Nexovent"
